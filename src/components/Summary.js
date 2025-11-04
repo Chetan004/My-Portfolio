fca763c3e7cf8cc2307b931c2ca7e73fffd24c47
chetan004 <112242587+Chetan004@users.noreply.github.com>
2025-11-04 09:13:06 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -14,10 +14,11 @@ function Summary() {
     >
       <h2 className="section-title mb-4">Summary</h2>
       <p className="summary-text lead">
-        DevOps Engineer with 1.3 year of hands-on experience in automating, deploying, and managing cloud-native applications on AWS. Skilled in Kubernetes, CI/CD pipelines, Docker, infrastructure automation using Terraform, and monitoring using Grafana and Prometheus. Proven ability to reduce cloud costs, enhance system availability, and streamline deployments across multiple environments.
+        DevOps Engineer with 2 year of hands-on experience in automating, deploying, and managing cloud-native applications on AWS. Skilled in Kubernetes, CI/CD pipelines, Docker, infrastructure automation using Terraform, and monitoring using Grafana and Prometheus. Proven ability to reduce cloud costs, enhance system availability, and streamline deployments across multiple environments.
       </p>
     </motion.section>
   );
 }
+
 
 export default Summary;
